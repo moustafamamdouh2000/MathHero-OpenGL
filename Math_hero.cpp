bcb674d172f79e5ed32c692be900d8a6fb2a4b5e
AbdoH7 <abdo.ah1102@gmail.com>
2022-06-06 16:34:08 +0200

    menu added

--- Math_hero.cpp
+++ Math_hero.cpp
@@ -16,9 +16,7 @@
 using namespace std;
 int PhyWidth = 700, PhyHeight = 700;
 int logWidth = 100, logHeight = 100;
-int centerX = logWidth/2, centerY =-3;
-int sqWidth=10;
-int sqHeight=6;
+int centerX = logWidth/2, centerY =logHeight/2;//-sqHiehgt/2
 int squareX=centerX,squareY=centerY;
 int mouseX,mouseY;
 bool negFlag=false;
@@ -27,32 +25,63 @@ int score=0;
 stringstream input;     // Input and answer from the user
 int userAnswer;     // to store the user answer and check if it was valid
 int disCount=-4;       // to know which problems are shown
+int flag=0;         //menu 0 , play 1
+void printSome(const char *str,int x,int y) {
+    glRasterPos2d(x,y);
+    for (int i=0;i<strlen(str);i++)
+        glutBitmapCharacter(GLUT_BITMAP_HELVETICA_18,str[i]);
+    glFlush();
+}
 
 class square{
 public:
-    int centerX=(logWidth/2);
-    int centerY=-3;
-    int width=10,height=6;
+    // int cX=(logWidth/2);
+    // int cY=-(height/2);
+    // int width=10,height=6;
+    int cX,cY,width,height;
     float red,green,blue;
     int alphaX=0,alphaY=0;
     char equation[10];
     int answer;
     bool isAnswered=false;
-    square(char * eq,int w,int h,float r,float g,float b,int cx,int cy){
+    bool isDisplayed=false;
+    square(const char * eq,int w,int h,int cx,int cy){
         width=w;
         height=h;
         strcpy(equation,eq);
-        red=r;
-        blue=b;
-        green=g;
-        centerX=cx;
-        centerY=cy;
+        cX=cx;
+        cY=cy;
     }
-    square(){};
+    square(int w,int h,int cx,int cy){
+        width=w;
+        height=h;
+        cX=cx;
+        cY=cy;
+    }
+    square (){}
+    int returnWid()
+    {
+        return width;
+    }
+    void drawSquare(int offset)
+    {
+        glBegin(GL_QUADS);
+        glVertex2d(cX-(width/2),cY-(height/2));
+        glVertex2d(cX+(width/2),cY-(height/2));
+        glVertex2d(cX+(width/2),cY+(height/2));
+        glVertex2d(cX-(width/2),cY+(height/2));
+        glEnd();
+        glColor3f(0,0,0);
+        printSome(equation,cX-offset,cY);
+    }
     bool checkRange(int x, int y){// check if point given is within the square
-        return x >= centerX-width/2 && x <= centerX+width/2 && y >= centerY-height/2 && y <= centerY+height/2;
+        return x >= cX-width/2 && x <= cX+width/2 && y >= cX-height/2 && y <= cY+height/2;
     }
 };
+square play=square("Play",20,15,centerX,centerY+25);
+square hs=square("High Scores",20,15,centerX,centerY);
+square ex=square("Exit",20,15,centerX,centerY-25);
+square background=square(logWidth,logHeight,centerX,centerY);
 
 class powerUpBird{
     public:
@@ -67,7 +96,15 @@ class powerUpBird{
 list <powerUpBird> birds;
 
 list <square> problems;
-
+    void lol1()
+    {
+        glColor3f(1,0,0);
+        glBegin(GL_LINE_LOOP);
+        glVertex2d(10,10);
+        glVertex2d(15,15);
+        glVertex2d(20,20);
+        glEnd();
+    }
 void checkAnswer(){
     for (int i=0;i<4;i++){
         list <square>::iterator it = problems.begin();
@@ -78,7 +115,6 @@ void checkAnswer(){
             printf("Answer %d is correct for problem no. %d your score is %d\n",userAnswer,i+disCount+1,score);
         }
     }
-    
 }
 
 void birdsGen(){ // needs further modefying to inssure that t2 is bigger than t1 at leaset by 15 seconds
@@ -127,7 +163,7 @@ void equationGen()
         }
         sprintf(eq,"%d %c %d",num1,operator_,num2);
         string myEq = eq;
-        square problem=square();
+        square problem=square(10,6,50,-3);
         //problem.equation=eq       //apparently this is wrong cause you can't assign to arrays -- they're not modifiable       oh god don't I love c
         strcpy(problem.equation, myEq.c_str());
         problem.answer=answerArr[i];
@@ -144,36 +180,25 @@ void drawVerticalLine(){
     glEnd();
 }
 
-void printSome(char *str,int x,int y) {
-    glRasterPos2d(x,y);
-    for (int i=0;i<strlen(str);i++)
-        glutBitmapCharacter(GLUT_BITMAP_HELVETICA_12,str[i]);
-    glFlush();
-}
 void drawBoxes(){
     for(int i=0;i<50;i++){
-
         list <square>::iterator it = problems.begin();
         advance(it,i);
         if(!it->isAnswered) glColor3f(1.0, 1.0, 1.0);
         else glColor3f(0.0,1.0,0.0);
-        //printf("yoloooooo  %d\t%d\n",it->centerX,it->centerY);
-        //printf("yoloooooozzzz  %d\t%d\n",it->alphaX,it->alphaY);
         glBegin(GL_QUADS);
-        glVertex2i(centerX-sqWidth,centerY+sqHeight+it->alphaY);
-        glVertex2i(centerX+sqWidth,centerY+sqHeight+it->alphaY);
-        glVertex2i(centerX+sqWidth,centerY-sqHeight+it->alphaY);
-        glVertex2i(centerX-sqWidth,centerY-sqHeight+it->alphaY);
+        glVertex2i(it->cX-it->width,it->cY+it->height+it->alphaY);
+        glVertex2i(it->cX+it->width,it->cY+it->height+it->alphaY);
+        glVertex2i(it->cX+it->width,it->cY-it->height+it->alphaY);
+        glVertex2i(it->cX-it->width,it->cY-it->height+it->alphaY);
         glEnd();
         glColor3f(0.0, 0.0, 0.0);
-        printSome(it->equation,centerX-sqWidth+2,centerY+it->alphaY);
-
-
+        printSome(it->equation,it->cX-it->width+5,it->cY+it->alphaY);
     }
-
 }
 
-void highScores(){
+
+void highScores(){//gets triggered after 60 seconds
     int s[10];
     string line;
     int i=0;
@@ -188,49 +213,54 @@ void highScores(){
     //write to scores files after sorting the arary
     scores.close();
 }
-
-
+char ans[10];
+void keyboard(unsigned char key,int x,int y){
+    if(!(key ==(char) 127 || key ==(char) 13 || (key >='0' && key<='9') || key=='-') || (key ==(char) 13 && input.rdbuf()->in_avail() == 0) )//not valid input
+    {
+        printf("Not valid input");
+        input.str(std::string());//clear
+    }
+    else{
+        if(key == (char) 127)//Delete Key, gotta figure out how to delete the key from the input itself , since Sstream is just gonna destory itself, or make it delete the whole thing
+        {
+            memset(ans,0,255);
+            keyCounter=0;
+            input.str(std::string());//clear
+        }
+        ans[keyCounter]=key;
+        if(((key>='0'&& key<='9') || key=='-')){
+            if(key=='-'&&!negFlag&&keyCounter==0){ 
+                input<<key;
+                negFlag=true;
+                keyCounter++;
+            }
+            else if (key>='0'&& key<='9'){
+                input<<key;
+                keyCounter++;
+            }
+        }
+        if(key == (char) 13)
+        {
+            userAnswer=stoi(input.str());
+            input.str("");//clear buffer
+            negFlag=false;
+            keyCounter=0;
+            memset(ans,0,255);
+            printf("%d\n",userAnswer);
+            checkAnswer();
+        }
+    }
+}
 void incr (){
     for (int i=0;i<50;i++){
         list <square>::iterator it = problems.begin();
         advance(it,i);
-        
         it->alphaY+=(logHeight/50);
 
     }
 }
-
-void keyboard(unsigned char key,int x,int y){
-    if((key>='0'&& key<='9') || key=='-'){
-        if(key=='-'&&!negFlag&&keyCounter==0){
-            input<<key;
-            negFlag=true;
-            keyCounter++;
-
-
-        }
-        else if (key>='0'&& key<='9'){
-          input<<key;
-          keyCounter++;
-        }
-    }
-    if(key == (char) 13)
-    {
-        userAnswer=stoi(input.str());
-        //string s=input.str();
-        // input.clear();
-        input.str("");//clear buffer
-        negFlag=false;
-        keyCounter=0;
-        //cout<<s<<endl;
-        printf("%d\n",userAnswer);
-        checkAnswer();
-        //userAnswer=0;
-    }
-}
-
 void mouseClick(int btn, int state, int x, int y){
-    if(btn==GLUT_LEFT_BUTTON && state==GLUT_DOWN)
+    if(btn==GLUT_LEFT_BUTTON && state==GLUT_DOWN && flag == 0)
     {
         mouseX = x;
         mouseX=0.5+1.0*mouseX*logWidth/PhyWidth;
@@ -246,18 +276,30 @@ void passiveMouse(int x,int y){
 }
 
 void checkAnswerTimer( int value ){
-    glutTimerFunc(2500, checkAnswerTimer, value);
+    if(value)
+    {
+        glutTimerFunc(2500, checkAnswerTimer, value);
         disCount++;
+    }
+    else
+    {
+        return ;
+    }
 }
 
 void displayTimer(int value){
-    glutTimerFunc(200, displayTimer, value);
-    incr();
-    glutPostRedisplay();
-    
-
-    
+    if(value)
+    {
+        glutTimerFunc(200, displayTimer, value);
+        incr();
+        glutPostRedisplay();
+    }
+    else
+    {
+        return ;//do nothing
+    }
 }
+
 
 void init(float Red,float Blue,float Green,float Alpha){
     glClearColor( Red,Green,Blue,Alpha );
@@ -265,18 +307,54 @@ void init(float Red,float Blue,float Green,float Alpha){
     gluOrtho2D( 0.0, logWidth, 0.0, logHeight);
 }
 
+char sc[10];
+
+// square play=square("Play",20,15,centerX,centerY+25); 
+void playGame()
+{
+    drawVerticalLine();// in play function
+    drawBoxes();
+    glColor3f(1,1,1);
+    sprintf(sc,"%s %d","Score: ",score);// PUT IN PLAY FUNC
+    printSome(sc,logWidth/10,logHeight/10);
+    printSome("Answer: ",logWidth-0.9*logWidth,logHeight-10);
+    printSome(ans,logWidth-0.9*logWidth + 10,logHeight-10);
+}
+
+void menu()//gets triggered with setup in the background
+{
+    glColor3f(1,1,1);
+    play.drawSquare(5);
+    glColor3f(1,1,1);
+    hs.drawSquare(7);
+    glColor3f(1,1,1);
+    ex.drawSquare(5);
+    if(play.checkRange(mouseX,mouseY))
+    {
+        flag=1;
+        checkAnswerTimer(1);
+        displayTimer(1);
+    }
+}
+
 void Display(){
     glClear( GL_COLOR_BUFFER_BIT );
-    drawVerticalLine();
-    drawBoxes();
-    char buff[10];
-    sprintf(buff,"%d",score);
-    glColor3f(1.0, 1.0, 1.0);
-    printSome(buff,5,10);
+    switch (flag)
+    {
+        case 0:menu();break;
+        case 1:playGame();break;
+    }
+    // drawVerticalLine();// in play function
+    // drawBoxes();
+    glColor3f(1,1,1);
+    // menu();
+    // printf("%d\n",play.cX);
+    // hs.drawSquare();
+    // ex.drawSquare();
+
     glFlush();
     glutSwapBuffers();
 }
-
 int main( int argc, char ** argv){
     glutInit( &argc, argv);
     glutInitDisplayMode( GLUT_DOUBLE | GLUT_RGB);
@@ -289,11 +367,10 @@ int main( int argc, char ** argv){
     glutPassiveMotionFunc(passiveMouse);
     glutKeyboardFunc(keyboard);
     //highScores();
+    memset(ans,0,255);
+    // checkAnswerTimer(0);
+    // displayTimer(0);
     birdsGen();
     equationGen();
-    Sleep(100);
-    checkAnswerTimer(0);
-    displayTimer(0);
     glutMainLoop();
 }
-
